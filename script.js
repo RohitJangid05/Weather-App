96207712fa09b6283ce97c8f59f15260c4ea6a94
RohitJangid05 <jangidrohit5820@gmail.com>
2024-03-20 22:39:57 +0530

    updated UI

--- script.js
+++ script.js
@@ -5,12 +5,17 @@ function convertUnixTimestampToTime(timestamp) {
     // Create a new Date object with the Unix timestamp in milliseconds
     const date = new Date(timestamp * 1000);
     // Get the hours, minutes, and seconds from the Date object
-    const hours = date.getHours();
+    let hours = date.getHours();
     const minutes = "0" + date.getMinutes();
     const seconds = "0" + date.getSeconds();
-    // Format the time as HH:MM:SS
-    return hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+    // Determine AM or PM
+    const meridiem = hours >= 12 ? 'PM' : 'AM';
+    // Convert hours to 12-hour format
+    hours = hours % 12 || 12;
+    // Format the time as HH:MM:SS AM/PM
+    return hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2) + ' ' + meridiem;
 }
+
 
 // Function to convert Unix timestamp to a date string
 function convertUnixTimestampToDate(timestamp) {
@@ -24,7 +29,6 @@ function convertUnixTimestampToDate(timestamp) {
     return day + '/' + month + '/' + year;
 }
 
-// Convert Unix timestamps to human-readable formats
 async function getWeather(city) {
     const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city;
 
@@ -58,8 +62,8 @@ async function getWeather(city) {
             Humidity.innerHTML = result.humidity + "%";
             Max_temp.innerHTML = result.max_temp + "°C";
             Min_temp.innerHTML = result.min_temp + "°C";
-            Sunrise.innerHTML = convertUnixTimestampToTime(result.sunrise) + " AM";
-            Sunset.innerHTML = convertUnixTimestampToTime(result.sunset) + " PM";
+            Sunrise.innerHTML = convertUnixTimestampToTime(result.sunrise) ;
+            Sunset.innerHTML = convertUnixTimestampToTime(result.sunset) ;
             Wind_degree.innerHTML = result.wind_degrees + "°";
             Wind_speed.innerHTML = result.wind_speed + " km/h";
             message.style.display='none'
